cc99ea38b98bceb2092378efae2ebcabfd39917c
Sachin Sanathkumar <sxs2449@student.bham.ac.uk>
2026-03-15 02:24:17 +0000

    added vuln

--- src/main/java/com/mycompany/myapp/config/SecurityConfiguration.java
+++ src/main/java/com/mycompany/myapp/config/SecurityConfiguration.java
@@ -59,6 +59,7 @@ public class SecurityConfiguration {
                 authz
                     .requestMatchers(HttpMethod.POST, "/api/scan-repository").permitAll() // ADD THIS LINE
                     .requestMatchers(HttpMethod.GET, "/api/smart-contracts/**").permitAll() // AND THIS FOR THE RESULTS
+                    .requestMatchers(HttpMethod.GET, "/api/vulnerabilities").permitAll()
                     .requestMatchers("/api/authenticate").permitAll()
                     .requestMatchers(mvc.pattern("/index.html"), mvc.pattern("/*.js"), mvc.pattern("/*.txt"), mvc.pattern("/*.json"), mvc.pattern("/*.map"), mvc.pattern("/*.css")).permitAll()
                     .requestMatchers(mvc.pattern("/*.ico"), mvc.pattern("/*.png"), mvc.pattern("/*.svg"), mvc.pattern("/*.webapp")).permitAll()
